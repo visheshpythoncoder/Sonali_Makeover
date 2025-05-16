6b601e660da721f29e07de6b9d7166190b68ab2f
unknown <vishesh maraskolhe>
2025-05-16 23:16:15 +0530

    Updated Google Maps embed src to new Saoner address

--- src/MakeUp-Pages/M_caontact.jsx
+++ src/MakeUp-Pages/M_caontact.jsx
@@ -162,11 +162,11 @@ export default function ContactComponent() {
               Location
             </h3>
             <p className="text-lg text-gray-700 mb-4">
-              Sai Temple, Saoner, Nagpur, Maharashtra
+            plot no 52,ward no.1,laxmi nagar,near janta lawn,chindwara road,saoner
             </p>
             <iframe
               title="Sai Temple Saoner Map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3715.243913839519!2d78.92432828077813!3d21.38029853285227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4e573b8a16c7f%3A0xe987aa54dce7e96f!2sSonali_Makeover%20Studio%20and%20Classes!5e0!3m2!1sen!2sin!4v1745305492381!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d404.78446943040916!2d78.91585324589126!3d21.39568937570323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1splot%20no%2052%2Cward%20no.1%2Claxmi%20nagar%2Cnear%20janta%20lawn%2Cchindwara%20road%2Csaoner!5e1!3m2!1sen!2sin!4v1747417140835!5m2!1sen!2sin" 
               width="100%"
               height="350"
               style={{ border: 0 }}
